4cd9491ec68684842c053dcc3ae7f1e9f34db597
HJOC15 <humbertorellana15@gmail.com>
2022-09-09 17:19:56 -0600

    Agregado el Soft Delete

--- models/nosql/storage.js
+++ models/nosql/storage.js
@@ -1,22 +1,20 @@
-const express = require("express");
-const { createItem, getItem, getItems, updateItem, deleteItem } = require("../controllers/storage");
-const uploadMiddleware = require("../utils/handleStorage");
-const { validatorGetItem } = require("../validators/storage");
-const router = express.Router();
+const mongoose = require("mongoose")
 
+const mongooseDelete = require("mongoose-delete");
+const StorageScheme = new mongoose.Schema(
+    {
+        url:{
+            type:String
+        },
+        filename:{
+            type:String
+        },
+    },
+    {
+        timestamps:true, //TODO createdAt , updatedAt
+        versionKey: false
+    }
+);
 
-//TODO http://localhost/api/storage GET,POST,DELETE,PUT
-
-//insertar item
-router.post("/",uploadMiddleware.single("myfile"),createItem);
-
-//obtener lista de items
-router.get("/",getItems)
-
-//obtener un solo item
-router.get("/:id",validatorGetItem,getItem)
-
-//eliminar item
-router.delete("/:id",validatorGetItem,deleteItem)
-
-module.exports = router; 
+StorageScheme.plugin(mongooseDelete,{overrideMethods: "all"});
+module.exports = mongoose.model("storage",StorageScheme)
