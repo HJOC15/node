00daeb6fc3036a94e3f875627f63ccaf8011d86d
HJOC15 <humbertorellana15@gmail.com>
2022-09-15 15:39:17 -0600

    JWT agregado

--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,7 @@
 const express = require("express");
-const {matchedData} = require("express-validator");
-const {userModel} = require("../models")
-const {encrypt,compare} = require("../utils/handlePassword")
+const { loginCtrl } = require("../controllers/auth");
 const { validatorRegister, validatorLogin } = require("../validators/auth");
 const router = express.Router();
 
-router.post("/register",validatorRegister, async (req,res) =>{
-    req = matchedData(req);
-    const passwordHash = await encrypt(req.password)
-    const body = {...req,password:passwordHash}
-    const data = await userModel.create(body)
-    data.set('password',undefined,{strict:false})
-    res.send({data})
-})
+router.post("/register",validatorRegister,loginCtrl)
 module.exports = router ;
